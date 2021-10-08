e2eaf721aeb160e4c40f5f5f6e1392aa8126642c
Christophe <christophe.misc+git@protonmail.ch>
2021-10-08 18:57:20 +0200

    Add message not found response

--- src/genericResponses.ts
+++ src/genericResponses.ts
@@ -3,5 +3,6 @@ const responses = {
     NO_PERM: '',
     NOT_TEXT: "That's no text channel",
     USER_NOT_FOUND: 'Wait, who are you?',
+    MESSAGE_NOT_FOUND: 'Could not find that message',
 };
 export default responses;
